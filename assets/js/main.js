95a8dd8e834246ab4dba8b320ea4c9401ace2f51
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-16 09:08:23 +0300

    first upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,57 @@
 jQuery(function ($) {
 
+    $(window).scroll(function () {
+      
+        scrollTop = $(window).scrollTop();
+
+        if (scrollTop > $('header').height()) {
+            $("#counter").css("padding-top", "150px");
+            $('header').addClass('scrollNav');
 
 
+        }
+
+        else {
+            $('header').removeClass('scrollNav');
+        }
+        if (($(window).scrollTop() + $(window).height()) > $(document).height()) {
+
+            $(".up").stop(true).animate({
+                opacity: 1
+            }, 100);
+
+        } else {
+
+            $(".up").stop(true).animate({
+                opacity: 0
+            }, 250);
+        }
+    
+    });
+
+    setTimeout(function () {
+        $('.EventCarusel').owlCarousel({
+          autoplay: true,
+          loop: false,
+          margin: 15,
+          nav:true,
+          responsive: {
+            0: {
+              items: 1,
+              nav:false,
+    
+            },
+            600: {
+              items: 2,
+            },
+            1000: {
+              items: 3,
+      
+            }
+       
+          }
+        });
+
+      }, 1000);
 });
 
