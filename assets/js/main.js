cf637864dc88969fbe30d483d38f51ef1b59e1a3
emanmoq <42995029+emanmoq@users.noreply.github.com>
2020-09-16 13:06:31 +0300

    ddd

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,7 @@ jQuery(function ($) {
         }
     
     });
+    $(".up").on("click", function () { $("html, body").animate({ scrollTop: 0 }, "slow"); return false; });
 
     setTimeout(function () {
         $('.EventCarusel').owlCarousel({
@@ -39,6 +40,7 @@ jQuery(function ($) {
             0: {
               items: 1,
               nav:false,
+             margin:0
     
             },
             600: {
@@ -46,11 +48,45 @@ jQuery(function ($) {
             },
             1000: {
               items: 3,
-      
             }
-       
           }
         });
+        $('.SponserSlider').owlCarousel({
+            autoplay: true,
+            loop: false,
+            margin: 15,
+            nav:true,
+            responsive: {
+              0: {
+                items: 1,
+                nav:false,
+      
+              },
+              600: {
+                items: 2,
+              },
+              1000: {
+                items: 4,
+        
+              }
+         
+            }
+          });
+          $('.TestimonialsCarusel').owlCarousel({
+            autoplay: true,
+            loop: false,
+            margin: 15,
+            nav:true,
+            items:1,
+            responsive: {
+              0: {
+             
+                nav:false,
+      
+              },
+         
+            }
+          });
 
       }, 1000);
 });
